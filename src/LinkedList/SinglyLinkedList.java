f7f71e08089e8b4426b9397e3471b534de407db3
Nomaan Ahmad <nomaan.ahmad@sequentum.com>
2024-11-10 20:18:31 +0530

    Some more addition to Singly Linked List

--- src/LinkedList/SinglyLinkedList.java
+++ src/LinkedList/SinglyLinkedList.java
@@ -19,7 +19,7 @@ public class SinglyLinkedList {
     public SinglyLinkedList() {
         count = 0;
     }
-    public boolean add(int val) {
+    public void add(int val) {
         Node node = new Node(val);
 
         if (head == null) head = node;
@@ -31,21 +31,72 @@ public class SinglyLinkedList {
             temp.next = node;
         }
 
+        traverse();
         this.count++;
-        return true;
     }
-    public boolean remove(){
-        if (head == null) return false;
-        else {
-            Node temp = head;
-            if (count == 1) head = null;
-            while(temp.next.next != null) temp = temp.next;
 
-            temp.next = null;
+    public void addAll(int[] arr)
+    {
+        if (arr.length == 0) return;
+
+        // Made a list containing all the array elements
+        Node tempHead = new Node(arr[0]);
+        Node tempTail = tempHead;
+        for (int i = 1; i < arr.length; i++){
+            tempTail.next = new Node(arr[i]);
+            tempTail = tempTail.next;
         }
 
-        count--;
-        return true;
+        if (head == null) head = tempHead;
+        else {
+            Node temp = head;
+            while (temp.next != null) temp = temp.next;
+
+            temp.next = tempHead;
+        }
+
+        this.count += arr.length;
+        traverse();
+    }
+
+    public void remove(int index) {
+        if (index < 0 || this.count <= index) throw new IndexOutOfBoundsException();
+
+        if (index == 0) {
+            if (count == 1) head = null;
+            else head = head.next;
+        }else if (index == count-1) {
+            int counter = 0;
+            Node temp = head;
+
+            while (++counter < index) temp = temp.next;
+            temp.next = null;
+        }else {
+            int counter = 0;
+            Node temp = head;
+
+            while (++counter < index) temp = temp.next;
+
+            temp.next = temp.next.next;
+        }
+
+        traverse();
+        this.count--;
+    }
+    public void remove(){
+        if (head == null) return;
+        else {
+            Node temp = head;
+            if (this.count == 1) head = null;
+            else{
+                while(temp.next.next != null) temp = temp.next;
+
+                temp.next = null;
+            }
+        }
+
+        traverse();
+        this.count--;
     }
     public void traverse(){
         if (head == null) System.out.println("[]");
