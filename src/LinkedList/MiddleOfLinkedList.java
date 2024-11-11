c85666a39563413c690b474ba9223bae2b1c4471
Nomaan Ahmad <nomaan.ahmad@sequentum.com>
2024-11-12 00:40:55 +0530

    Middle of a linked list

---
+++ src/LinkedList/MiddleOfLinkedList.java
@@ -0,0 +1,24 @@
+package LinkedList;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+public class MiddleOfLinkedList {
+    public ListNode middleNode(ListNode head) {
+        if (head == null) return null;
+        
+        ListNode first = head;
+        ListNode second = head;
+
+        while(second != null && second.next != null) {
+            first = first.next;
+            second = second.next.next;
+        }
+
+        return first;
+    }
+}
