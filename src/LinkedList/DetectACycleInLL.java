e9292d1cadacd8bc5aae339e8df252bc44fe9fb4
Nomaan Ahmad <nomaan.ahmad@sequentum.com>
2024-11-12 17:04:54 +0530

    Detect a cycle in a linked list

---
+++ src/LinkedList/DetectACycleInLL.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+public class DetectACycleInLL {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) return true;
+        }
+
+        return false;
+    }
+}
