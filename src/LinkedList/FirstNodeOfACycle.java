b7da1893eb2d1b13dc5d4ac83f480c3912ee7348
Nomaan Ahmad <nomaan.ahmad@sequentum.com>
2024-11-12 18:04:29 +0530

    First node of a loop in a linked list

---
+++ src/LinkedList/FirstNodeOfACycle.java
@@ -0,0 +1,31 @@
+package LinkedList;
+
+public class FirstNodeOfACycle {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) break;
+        }
+
+        if (slow == fast)
+        {
+            slow = head;
+
+            while (slow != fast){
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+            return slow;
+
+        }else return null;
+    }
+}
