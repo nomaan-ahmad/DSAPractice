f4fd397072ca826d8c609d3009eeb653a9ce926e
Nomaan Ahmad <nomaan.ahmad21@gmail.com>
2025-02-04 01:10:38 +0530

    Leetcode Daily | Longest strictly increasing or decreasing subarray

---
+++ src/Array/LongestStrictlyIncreasingOrStrictlyDecreasing.java
@@ -0,0 +1,24 @@
+package Array;
+
+// https://leetcode.com/problems/longest-strictly-increasing-or-strictly-decreasing-subarray/
+public class LongestStrictlyIncreasingOrStrictlyDecreasing {
+    public int longestMonotonicSubarray(int[] nums) {
+        int maxLength = 1;
+        int incLength = 1;
+        int decLength = 1;
+        for(int pos = 0; pos < nums.length - 1; pos++) {
+            if(nums[pos + 1] > nums[pos]) {
+                incLength++;
+                decLength = 1;
+            } else if(nums[pos + 1] < nums[pos]) {
+                decLength++;
+                incLength = 1;
+            } else {
+                incLength = 1;
+                decLength = 1;
+            }
+            maxLength = Math.max(maxLength, Math.max(incLength, decLength));
+        }
+        return maxLength;
+    }
+}
