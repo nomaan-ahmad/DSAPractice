eeb10f8cd625e0c539388c560c01924bc156c7ab
Nomaan Ahmad <nomaan.ahmad21@gmail.com>
2025-03-29 01:04:27 +0530

    Array | Valid Palindrome II

--- Array/ValidPalindrome_II.java
+++ Array/ValidPalindrome_II.java
@@ -1,4 +1,33 @@
 package Array;
 
+// https://leetcode.com/problems/valid-palindrome-ii/
+
 public class ValidPalindrome_II {
+    public static void main(String[] args) {
+        String s = "eceec";
+        System.out.println(validPalindrome(s));
+    }
+
+    public static boolean validPalindrome(String s) {
+        int temp = isPalindrome(s, 0, s.length()-1);
+
+        if (temp != -1) {
+            int temp1 = isPalindrome(s, temp, s.length()-temp-2);
+            int temp2 = isPalindrome(s, temp+1, s.length()-temp-1);
+
+            return temp1 == -1 || temp2 == -1;
+        }
+        else return true;
+    }
+
+    private static int isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) return start;
+
+            start++;
+            end--;
+        }
+
+        return -1;
+    }
 }
